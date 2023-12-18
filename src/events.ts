b0818820a75546e90a59ad05e346043dbc5f9f15
mooujarrar <mohyiddine1997@gmail.com>
2023-12-18 12:32:13 +0100

    enh# extend events list

---
+++ src/events.ts
@@ -0,0 +1,5 @@
+export const CREATE_SENSOR = 'sensor.create';
+export const DELETE_SENSOR = 'sensor.delete';
+export const UPDATE_SENSOR = 'sensor.delete';
+export const RETRIEVE_SENSOR = 'sensor.retrieve';
+export const SAVE_SENSOR_DATA = 'sensor.data.save';
