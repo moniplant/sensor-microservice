75285ede5317b5b8bdff75b7712a7d01bdecdaf3
mooujarrar <mohyiddine1997@gmail.com>
2024-06-03 17:55:43 +0200

    maint update events

--- src/events.ts
+++ src/events.ts
@@ -1,5 +1,10 @@
+// SENSOR API - Kafka
 export const CREATE_SENSOR = 'sensor.create';
 export const DELETE_SENSOR = 'sensor.delete';
-export const UPDATE_SENSOR = 'sensor.delete';
+export const UPDATE_SENSOR = 'sensor.update';
 export const RETRIEVE_SENSOR = 'sensor.retrieve';
+export const LIST_PLANT_SENSORS = 'sensors.plant.list';
+export const DELETE_PLANT_SENSORS = 'sensors.plant.delete';
+
+// SENSOR DATA API - MQTT
 export const SAVE_SENSOR_DATA = 'sensor.data.save';
