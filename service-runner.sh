c329dff8ff8b782e62eaa1e4245ccaf129d857bc
mooujarrar <mohyiddine1997@gmail.com>
2025-04-30 17:26:12 +0200

    enh# correct sensor service CI/CD for new rpi5

--- service-runner.sh
+++ service-runner.sh
@@ -4,6 +4,9 @@ ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
   echo "Detected architecture: armv7l (ARM). Using armhf-specific override file."
   docker compose -f docker-compose.yaml -f docker-compose.override.armhf.yaml up -d
+elif [ "$ARCH" = "aarch64" ]; then
+  echo "Detected architecture: aarch64 (ARM64, Raspberry Pi 5). Running in production mode and skipping selected builds."
+  docker compose --profile prod -f docker-compose.yaml -f docker-compose.override.aarch64.yaml up -d
 else
   echo "Detected architecture: $ARCH. Using default configuration."
   docker compose up -d
